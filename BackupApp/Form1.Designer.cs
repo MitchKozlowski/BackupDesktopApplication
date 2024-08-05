baadf1c4a053dbbc5de75ffa3a8dbbfaae26562e
MitchKozlowski <57924574+MitchKozlowski@users.noreply.github.com>
2024-08-05 05:09:51 -0400

    Add files via upload

---
+++ BackupApp/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BackupApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonBackup = new System.Windows.Forms.Button();
+            this.buttonSelectDir = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.textBoxBackupDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.AllowDrop = true;
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 16;
+            this.listBoxFiles.Location = new System.Drawing.Point(68, 42);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(196, 260);
+            this.listBoxFiles.TabIndex = 0;
+            // 
+            // buttonBackup
+            // 
+            this.buttonBackup.Location = new System.Drawing.Point(12, 377);
+            this.buttonBackup.Name = "buttonBackup";
+            this.buttonBackup.Size = new System.Drawing.Size(113, 63);
+            this.buttonBackup.TabIndex = 1;
+            this.buttonBackup.Text = "Commit Backup";
+            this.buttonBackup.UseVisualStyleBackColor = true;
+            // 
+            // buttonSelectDir
+            // 
+            this.buttonSelectDir.Location = new System.Drawing.Point(225, 375);
+            this.buttonSelectDir.Name = "buttonSelectDir";
+            this.buttonSelectDir.Size = new System.Drawing.Size(113, 65);
+            this.buttonSelectDir.TabIndex = 2;
+            this.buttonSelectDir.Text = "Choose Backup Directory";
+            this.buttonSelectDir.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(270, 119);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(105, 40);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "Remove Selected";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // textBoxBackupDir
+            // 
+            this.textBoxBackupDir.Location = new System.Drawing.Point(12, 329);
+            this.textBoxBackupDir.Name = "textBoxBackupDir";
+            this.textBoxBackupDir.Size = new System.Drawing.Size(326, 22);
+            this.textBoxBackupDir.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Drag and Drop files to back them up:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Backup Directory Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 498);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBackupDir);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonSelectDir);
+            this.Controls.Add(this.buttonBackup);
+            this.Controls.Add(this.listBoxFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonBackup;
+        private System.Windows.Forms.Button buttonSelectDir;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.TextBox textBoxBackupDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BackupApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackupApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBoxFiles.AllowDrop = true;
+            listBoxFiles.DragEnter += ListBoxFiles_DragEnter;
+            listBoxFiles.DragDrop += ListBoxFiles_DragDrop;
+            buttonBackup.Click += ButtonBackup_Click;
+            buttonSelectDir.Click += ButtonSelectDir_Click; // Optional for selecting directory
+            buttonRemove.Click += ButtonRemove_Click; // Handle the remove button click event
+        }
+
+        private void ListBoxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void ListBoxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    listBoxFiles.Items.Add(file);
+                }
+            }
+        }
+
+        private void ButtonBackup_Click(object sender, EventArgs e)
+        {
+            string backupDir = textBoxBackupDir.Text;
+
+            if (string.IsNullOrWhiteSpace(backupDir) || !Directory.Exists(backupDir))
+            {
+                MessageBox.Show("Please select a valid backup directory.");
+                return;
+            }
+
+            foreach (string filePath in listBoxFiles.Items)
+            {
+                string fileName = Path.GetFileName(filePath);
+                string destPath = Path.Combine(backupDir, fileName);
+
+                try
+                {
+                    File.Copy(filePath, destPath, true);
+                    MessageBox.Show($"File '{fileName}' backed up successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error backing up file '{fileName}': {ex.Message}");
+                }
+            }
+        }
+
+        private void ButtonSelectDir_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxBackupDir.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            // Remove selected items from the listBox
+            var selectedItems = listBoxFiles.SelectedItems.OfType<string>().ToList();
+            foreach (var item in selectedItems)
+            {
+                listBoxFiles.Items.Remove(item);
+            }
+        }
+    }
+}
